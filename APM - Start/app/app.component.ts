c04c20b384b409a8857b29902ea520bc055a9115
Aneudy Guzman <aneguzman@gmail.com>
2016-06-12 00:14:48 -0400

    Adding productList component

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from 'angular2/core'
 
 @Component({
 	selector: 'pm-app',
-	template: '<div>Hello World</div>'
+	template: '<div><h1>{{pageTitle}}</h1><div>My first Component</div></div>'
 })
 export class AppComponent {
 	pageTitle: string = "Acme Product Managment";
