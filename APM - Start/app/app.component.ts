90b7fd002006dc98f3a8fcb6192f63a1d9cfe07d
Aneudy Guzman <aneguzman@gmail.com>
2016-06-16 02:01:01 -0400

    Adding Routing and navigation

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -3,14 +3,36 @@ import { ProductListComponent } from './products/product-list.component';
 import { ProductService} from './products/product.service';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import 'rxjs/Rx';
+import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+
 
 
 @Component({
 	selector: 'pm-app',
-	template: '<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>',
-	directives: [ProductListComponent],
-	providers: [ProductService, HTTP_PROVIDERS]
+	template: `
+	<div>
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<a class="navbar-brand">{{pageTitle}}</a>
+				<ul class="nav navbar-nav">
+					<li><a [routerLink]="['Welcome']">Home</a></li>
+					<li><a [routerLink]="['Products']">Products</a></li>
+				</ul>
+			</div>
+		</nav>
+	</div>	
+	<router-outlet></router-outlet>
+	`,
+	directives: [ROUTER_DIRECTIVES],
+	providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
+@RouteConfig([
+	{ path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true },
+	{ path: '/products', name: 'Products', component: ProductListComponent },
+	{ path: '/product/:id', name: 'ProductDetail', component: ProductDetailComponent }
+])
 export class AppComponent {
 	pageTitle: string = "Acme Product Managment";
 }
